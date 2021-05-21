b7c6f8e9d15dd19c19976ce12550d30178430a5f
Santiago Cronenberg <santicronen@gmail.com>
2021-05-21 14:06:45 -0300

    Commit

--- vinoteca/abm_alta.php
+++ vinoteca/abm_alta.php
@@ -1,41 +1,54 @@
 <html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>    
     <body>
+        <body class="align">
+        <header class="_header">
+            <img src="/vinoteca/logo_circular.png" width="150" height="150">
+            <p></p>
+            <h3 class="_title">STOCK VINOTECA</h3>
+        </header>
+
         <?php include "conexion.php"; ?>
-
+        <div class="_body">
         <form name=alta action="abm_alta_db.php" method="get">
-            <h2>ALTA DE PRODUCTO</h2>
-            <table>
-                <tr>
-                    <td>Codigo de producto</td>
-                    <input type="number" name=productoID>
-                    <br><br>
+            <h3 class="_title">ALTA DE PRODUCTO</h3>
+                <label for="var">Código de producto</label><br>
+                <input type="number" id="var" name=productoID>
+                <br>
 
-                    <td>Nombre de producto</td>
-                    <input type="text" name=productoNombre>
-                    <br><br>
+                <label for="var">Nombre de producto</label><br>
+                <input type="text" id="var" name=productoNombre required>
+                <br>                
+                <label for="var">Empresa</label><br>
 
-                    <td>Empresa de producto</td>
-                    CONSULTA
-                    <select name="empresaID">
-                        <?php
-                        $sql = "SELECT * FROM empresa ORDER BY empresaID";
+                <select name="empresaID">
+                    <?php
+                    $sql = "SELECT * FROM empresa ORDER BY empresaID ASC";              
+                    $resu = mysqli_query($conexion,$sql) or die ($sql . mysqli_error($conexion));               
+                    while($a = mysqli_fetch_array($resu))
+                    {
+                    ?>
+                            <option value='<?php echo $a['empresaID']; ?>'><?php echo $a['empresaNombre']; ?></option>
+                    <?php
+                }?>
+                </select>
 
-                        $resu = mysqli_query($conexion,$sql) or die ($sql . mysqli_error($conexion));
+                <br><label for="var">Costo de producto</label><br>
+                    <input type="number" id="var" name=productoCosto>
+                
 
-                        while($a = mysqli_fetch_array($resu))
-                        ?>
-                        {
-                                <option value='<?php echo $a['empresaID']; ?>'><?php echo $a['empresaNombre']; ?></option>
-                    </select>
-                </tr>
-                <tr>
-                    <td><input type="submit" name="enviar" value="Enviar"></td>
-                </tr>
-                <script>
-                    alert("Se agregó un nuevo producto.");
-                    location.replace("abm.html");
-                </script>
-            </table>
+                <br><label for="var">Precio de producto</label><br>
+                    <input type="number" id="var" name=productoPrecio>
+
+                <br><label for="var">Stock inicial de producto</label><br>
+                    <input type="number" id="var" name=productoStock>
+                    
+
+                <br><br><input type="submit" name="enviar" value="Enviar">
+                <input type="button" name="volver" value="Volver al menu principal" onclick="location.replace('abm.html');">
         </form>
+        </div>
     </body>
 </html>
--- vinoteca/consulta.php
+++ vinoteca/consulta.php
@@ -1,9 +1,28 @@
 <html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
     <body>
+    <body class="align">
+        <header class="_header">
+            <img src="/vinoteca/logo_circular.png" width="150" height="150">
+            <p></p>
+            <h3 class="_title">LISTA DE PRODUCTOS</h3>
+        </header>
+        <div class="_body">
     <?php include "conexion.php"; ?>
-        <table>
-            <h2>Lista de productos</h23>
-            <td> 
+        <table style="width: auto">
+        
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Costo</th>
+                <th>Precio</th>
+                <th>Stock</th>
+                <th>Modificar</th>
+                <th>Eliminar</th>
+            </tr>
+            
                 <?php
                 $sql = "SELECT * FROM producto ORDER BY productoID";
 
@@ -11,22 +30,21 @@
 
                 while ($a = mysqli_fetch_array($resu))
                 {
-                    ?>
-                    <br>
-                    <?php
-                    echo $a['productoID']; 
-                    echo ' - ';
-                    echo $a['productoNombre'];
-                    echo ' | Stock: ';
-                    echo $a['productoStock']
-                    ?>
-                    <br>  
-                <?php
+                    ?><tr>
+                    <td><?php echo $a['productoID'];?></td>
+                    <td><?php echo $a['productoNombre'];?></td>
+                    <td><?php echo $a['productoCosto'];?></td>
+                    <td><?php echo $a['productoPrecio'];?></td>
+                    <td><?php echo $a['productoStock'];?></td>
+                    <td><input type="button" name="modificar" value="Modificar" onclick="location.replace('amb_mod.php');"</td>
+                    <td><input type="button" name="eliminar" value="Eliminar" onclick="location.replace('amb_baja.php');"</td>
+                </tr><?php
                 }
                 ?>
-            </td>
-        </table>
-        <br>
+            
+        </table><br>
         <input type="button" name="volver" value="Volver al menu principal" onclick="location.replace('index.html');">
+        </div>
+        <br>
     </body>
 </html>
